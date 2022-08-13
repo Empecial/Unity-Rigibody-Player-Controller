5ab0930e853668411237d3c31bc7f6357d631abb
Empecial <empecial@gmail.com>
2022-08-13 14:06:42 +0200

    initial upload

---
+++ Assets/GroundCheck.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheck : MonoBehaviour
+{
+
+    public PlayerController playercontroller;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == playercontroller.gameObject)
+        {
+            return;
+        }
+
+        playercontroller.SetGrounded(true);
+
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == playercontroller.gameObject)
+        {
+            return;
+        }
+
+        playercontroller.SetGrounded(false);
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject == playercontroller.gameObject)
+        {
+            return;
+        }
+
+        playercontroller.SetGrounded(true);
+
+    }
+
+
+}
